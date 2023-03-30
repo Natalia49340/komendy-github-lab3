3008cda9b1300ff1f85a3dd6784ef379475756de
Natalia49340 <dsw49340@student.dsw.edu.pl>
2023-03-30 17:24:45 +0200

    commit2

---
+++ Lab3.sh
@@ -0,0 +1,37 @@
+git commit
+git branch
+git checkout <nazwa-gałęzi>
+git checkout -b <nazwa-gałęzi>
+git merge <nazwa-gałęzi>
+git rebase <nazwa-gałęzi>
+git log
+git checkout^
+git checkout HEAD~(num)
+git branch -f <nazwa-gałęzi> HEAD~(num)
+git reset
+git revert HEAD
+git cherry-pick <Commit1><Commmit2><...>
+git rebase -i
+git commit --amend
+git describe <ref>
+git bisect
+git tag
+
+git clone
+o/<nazwa-gałęzi>
+git fetch
+git cherry-pick o/main
+git rebase o/main
+git merge o/main
+git pull
+git fakeTeamwork
+git push
+git pull --rebase
+git checkout -b <nazwa gałęzi> o/main
+git branch -u
+git branch -u o/main
+git push origin main
+git push origin <source>:<destination>
+git fetch origin 
+git push origin :<nazwa-gałęzi>
+git fetch origin : <nazwa-gałęzi>
